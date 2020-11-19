bb7eb934f33eadc6cc6ab1276bcddd72a4a41b77
hjhgitw <hansjoerg.hofer@outlook.com>
2020-11-19 13:18:41 +0100

    sec

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,8 @@
 import { OAuth2PopupFlow } from 'oauth2-popup-flow';
-
+//%%CLIENT_ID%%
 export default new OAuth2PopupFlow({
   authorizationUri: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
-  clientId: '%%CLIENT_ID%%',
+  clientId: 'CUFgxbQTS8mb2Ei88s8EN9YR7wt7NWjr',
   redirectUri: 'https://help-scout.netlify.app/redirect',
   scope: 'openid profile',
   responseType: 'id_token',
