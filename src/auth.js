ef0e06d2c7d5054a20911b2236f251780a5fe2d0
hjhgitw <hansjoerg.hofer@outlook.com>
2020-11-19 11:13:44 +0100

    fix url

--- src/auth.js
+++ src/auth.js
@@ -2,8 +2,8 @@ import { OAuth2PopupFlow } from 'oauth2-popup-flow';
 
 export default new OAuth2PopupFlow({
   authorizationUri: 'https://secure.helpscout.net/authentication/authorizeClientApplication',
-  clientId: 'CUFgxbQTS8mb2Ei88s8EN9YR7wt7NWjr',
-  redirectUri: 'http://localhost:8080/redirect',
+  clientId: '%%CLIENT_ID%%',
+  redirectUri: 'https://help-scout.netlify.app/redirect',
   scope: 'openid profile',
   responseType: 'id_token',
   accessTokenResponseKey: 'id_token',
